b2b15aee47aa5c39346d26454150f4c81fb3ad40
JY F <fengjianyongcn@gmail.com>
2026-02-27 11:28:34 +0800

    readme and some dependencies

---
+++ src/main/java/com/jerryfeng/sbo/controller/HomeController.java
@@ -0,0 +1,11 @@
+package com.jerryfeng.sbo.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "Welcome to the home page!";
+    }
+}
